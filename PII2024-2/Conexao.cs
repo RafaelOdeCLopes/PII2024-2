3bc12c20fa019f1000f8ac1651b44dd0298eed60
RafaelOdeCLopes <152555858+RafaelOdeCLopes@users.noreply.github.com>
2024-10-23 12:19:18 -0300

    Criação da classe de conexão para o SQL Server.

---
+++ PII2024-2/Conexao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PII2024_2
+{
+    internal class Conexao
+    {
+        SqlConnection conn = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        public SqlConnection Conn { get => conn; set => conn = value; }
+        public SqlCommand Cmd { get => cmd; set => cmd = value; }
+
+        public void Conectar()
+        {
+            string aux = "Server=.\\SQLEXPRESS;Database=BancoPII;UID=sa;PWD=senha123";
+            Conn.ConnectionString = aux;
+            Conn.Open();
+        }
+        public void Desconectar()
+        {
+            Conn.Close();
+        }
+        public void Executar(string sql)
+        {
+            Cmd.Connection = Conn;
+            Cmd.CommandText = sql;
+            Cmd.ExecuteNonQuery();
+        }
+    }
+}
