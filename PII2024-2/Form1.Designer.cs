7b993a8788ed5106c33c510687e0c1bbf8dc4ae2
RafaelOdeCLopes <152555858+RafaelOdeCLopes@users.noreply.github.com>
2024-10-31 10:58:38 -0300

    Alterações no design do formulário de abertura.

--- PII2024-2/Form1.Designer.cs
+++ PII2024-2/Form1.Designer.cs
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTelaInicial1 = new System.Windows.Forms.Label();
+            this.lblTelaInicial2 = new System.Windows.Forms.Label();
+            this.mnsTelaInicial = new System.Windows.Forms.MenuStrip();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsTelaInicial.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTelaInicial1
+            // 
+            this.lblTelaInicial1.BackColor = System.Drawing.Color.Transparent;
+            this.lblTelaInicial1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelaInicial1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(98)))), ((int)(((byte)(57)))));
+            this.lblTelaInicial1.Location = new System.Drawing.Point(12, 162);
+            this.lblTelaInicial1.Name = "lblTelaInicial1";
+            this.lblTelaInicial1.Size = new System.Drawing.Size(776, 49);
+            this.lblTelaInicial1.TabIndex = 0;
+            this.lblTelaInicial1.Text = "FOME ZERO";
+            this.lblTelaInicial1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTelaInicial2
+            // 
+            this.lblTelaInicial2.BackColor = System.Drawing.Color.Transparent;
+            this.lblTelaInicial2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold);
+            this.lblTelaInicial2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(98)))), ((int)(((byte)(57)))));
+            this.lblTelaInicial2.Location = new System.Drawing.Point(12, 235);
+            this.lblTelaInicial2.Name = "lblTelaInicial2";
+            this.lblTelaInicial2.Size = new System.Drawing.Size(776, 49);
+            this.lblTelaInicial2.TabIndex = 1;
+            this.lblTelaInicial2.Text = "PII VIII";
+            this.lblTelaInicial2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mnsTelaInicial
+            // 
+            this.mnsTelaInicial.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnsTelaInicial.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.mnsTelaInicial.Location = new System.Drawing.Point(0, 0);
+            this.mnsTelaInicial.Name = "mnsTelaInicial";
+            this.mnsTelaInicial.Size = new System.Drawing.Size(800, 28);
+            this.mnsTelaInicial.TabIndex = 2;
+            this.mnsTelaInicial.Text = "menuStrip1";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(48, 24);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PII2024_2.Properties.Resources.FomeZero;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lblTelaInicial2);
+            this.Controls.Add(this.lblTelaInicial1);
+            this.Controls.Add(this.mnsTelaInicial);
+            this.MainMenuStrip = this.mnsTelaInicial;
+            this.Name = "Form1";
+            this.Text = "Tela inicial";
+            this.mnsTelaInicial.ResumeLayout(false);
+            this.mnsTelaInicial.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTelaInicial1;
+        private System.Windows.Forms.Label lblTelaInicial2;
+        private System.Windows.Forms.MenuStrip mnsTelaInicial;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
     }
 }
 
