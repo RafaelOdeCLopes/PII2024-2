247a5f485a53f6a09a539c4e05d436856c68b867
Tallesdev <149217048+Tallesdev@users.noreply.github.com>
2024-11-04 21:25:58 -0300

    Cadastro Empresa Fabricante forms

---
+++ PII2024-2/Cadastro_EmpresaFabricante.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PII2024_2
+{
+    public partial class Cadastro_EmpresaFabricante : Form
+    {
+        public Cadastro_EmpresaFabricante()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_NomeFabricante_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_TelefoneFabricante_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ConfirmarCadastroFabricante_Click(object sender, EventArgs e)
+        {
+            EmpresasFabricantes EmpFab = new EmpresasFabricantes();
+            EmpFab.NomeEmpresa = txt_NomeFabricante.Text;
+            EmpFab.Endereco = txt_EnderecoFabricante.Text;
+            EmpFab.Telefone = txt_TelefoneFabricante.Text;
+
+            MessageBox.Show(EmpFab.NomeEmpresa);
+        }
+    }
+}
