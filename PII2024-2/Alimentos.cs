1a69fb0ca74e42d8fc7d4fa47665b9dcddbe5731
RafaelOdeCLopes <152555858+RafaelOdeCLopes@users.noreply.github.com>
2024-11-18 09:27:02 -0300

    Alterações e complementos na funcionalidade de inserção da tabela "Alimentos".

--- PII2024-2/Alimentos.cs
+++ PII2024-2/Alimentos.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlTypes;
 
 namespace PII2024_2
 {
     internal class Alimentos
     {
+        SQLServer sql = new SQLServer();
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
@@ -15,6 +17,26 @@ namespace PII2024_2
         public int Quantidade { get; set; }
         public string Origem { get; set; }
         public int IdEmpresaFabricante { get; set; }
-        public int IdDoacao { get; set; }
+        public SqlInt16 IdDoacao { get; set; }
+        public void Inserir()
+        {
+            string query = @"INSERT INTO alimentos 
+                     (nome, descricao, data_validade, quantidade, origem, id_empresa_fabricante, id_doacao)
+                     VALUES 
+                     (@Nome, @Descricao, @DataValidade, @Quantidade, @Origem, @IdEmpresaFabricante, @IdDoacao)";
+
+            var parametros = new Dictionary<string, object>
+            {
+                {"@Nome", Nome},
+                {"@Descricao", Descricao},
+                {"@DataValidade", DataValidade},
+                {"@Quantidade", Quantidade},
+                {"@Origem", Origem},
+                {"@IdEmpresaFabricante", IdEmpresaFabricante},
+                {"@IdDoacao", IdDoacao.IsNull ? (object)DBNull.Value : IdDoacao.Value}
+            };
+            sql.ExecutarComParametros(query, parametros);
+        }
+
     }
 }
--- PII2024-2/CadastroAlimentos.cs
+++ PII2024-2/CadastroAlimentos.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlTypes;
 
 namespace PII2024_2
 {
@@ -14,11 +16,60 @@ namespace PII2024_2
     {
         bool sidebarExpand;
         bool homeCollapsed;
+        SQLServer sql = new SQLServer();
+        Alimentos alimento = new Alimentos();
         public CadastroAlimentos()
         {
             InitializeComponent();
+            sql.Conectar();
+            using (SqlCommand cmd = new SqlCommand("SELECT id_empresa_fabricante FROM alimentos", sql.Conn))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string idEmpresa = reader["id_empresa_fabricante"].ToString();
+                        if (!cmbEmpresa.Items.Contains(idEmpresa))
+                        {
+                            cmbEmpresa.Items.Add(idEmpresa);
+                        }
+                    }
+                }
+            }
+            using (SqlCommand cmd = new SqlCommand("SELECT id_doacao FROM alimentos", sql.Conn))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string idDoacao = reader["id_doacao"].ToString();
+                        if (!cmbDoacao.Items.Contains(idDoacao))
+                        {
+                            cmbDoacao.Items.Add(idDoacao);
+                        }
+                    }
+                }
+            }
         }
 
-      
+        private void btnSalvarcadastroalimentos_Click(object sender, EventArgs e)
+        {
+            alimento.Nome = txtNome.Text;
+            alimento.Descricao = txtDescricao.Text;
+            alimento.DataValidade = DateTime.Parse(mtbdata.Text);
+            alimento.Quantidade = int.Parse(txtQuantidade.Text);
+            alimento.Origem = cmbOrigem.SelectedItem.ToString();
+            alimento.IdEmpresaFabricante = int.Parse(cmbEmpresa.SelectedItem.ToString());
+            if (alimento.Origem == "Compra")
+            {
+                alimento.IdDoacao = SqlInt16.Null;
+            }
+            else
+            {
+                alimento.IdDoacao = SqlInt16.Parse(cmbDoacao.Text.ToString());
+            }
+            alimento.Inserir();
+            MessageBox.Show("Alimento inserido com sucesso!");
+        }
     }
 }
